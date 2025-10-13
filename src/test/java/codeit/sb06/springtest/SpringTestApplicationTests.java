877e8eef8e03f24b6150c7573e703041a473a2aa
hagyutae <gted221@gmail.com>
2025-10-13 15:20:52 +0900

    project init

---
+++ src/test/java/codeit/sb06/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.sb06.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
